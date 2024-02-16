a7205f8db1bc8f1c88b80bf418f904cbe9d2f449
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2024-02-16 11:59:08 -0300

    feat: message for loan

--- src/main/java/br/com/virtual/threads/Application.java
+++ src/main/java/br/com/virtual/threads/Application.java
@@ -2,6 +2,7 @@ package br.com.virtual.threads;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
 
 @SpringBootApplication
 public class Application {
@@ -11,3 +12,20 @@ public class Application {
 	}
 
 }
+
+sealed interface Loan permits SecuredLoan, UnsecuredLoan {}
+
+record SecuredLoan(int value) implements Loan {}
+
+record UnsecuredLoan() implements Loan {}
+
+class Message {
+	String messageForLoan(Loan loan) {
+		return switch (loan) {
+			case UnsecuredLoan unsecuredLoan -> "";
+			case SecuredLoan (var value) -> "" + value;
+		};
+	}
+}
+
+record Customer (@Id Integer id, String name) {}
